12847b3c12d2824ae27ff8c5bee7fb050a78414a
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-05-26 21:38:57 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,1 +1,30 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+	string name;
+	int age;
+
+public:
+	Student() : name("NAMELESS OMG"), age(0) {}
+	Student(string name, int age) : name(name), age(age) {}
+
+	bool operator < (const Student& student) const {
+		return age < student.age;
+	};
+
+	friend istream& operator >> (istream& i, Student& student) {
+		cout << "Enter name: ";
+		i >> student.name;
+		cout << "Enter age: ";
+		i >> student.age;
+		return i;
+	}
+
+	friend ostream& operator << (ostream& o, const Student& student) {
+		o << "Name: " << student.name << "\n" << "Age: " << student.age;
+		return o;
+	}
+};
